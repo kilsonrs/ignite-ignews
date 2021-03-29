aae2c223a9acf64e4cd26d45831f3c96db0f8512
Kilson <kilsonrs@gmail.com>
2021-03-29 11:08:42 -0300

    first-commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
+import Head from 'next/head'
+
+
 export default function Home() {
   return (
-    <h1>Hello</h1>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>
+        Hello World
+      </h1>
+    </>
   )
 }
